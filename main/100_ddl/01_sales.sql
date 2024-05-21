39cc5240af3088cf7eec72ffa02d6beeea2e124c
Adeel Malik <amalik@datical.com>
2024-05-21 16:49:25 -0500

    first commit

---
+++ main/100_ddl/01_sales.sql
@@ -0,0 +1,5 @@
+create table sales (
+    id int,
+    first_name varchar (50),
+    last_name varchar (50)
+);
---
+++ main/100_ddl/07_exemployee.sql
@@ -0,0 +1,5 @@
+CREATE TABLE exemployee (
+   id  NUMBER(5), 
+   name VARCHAR2(30), 
+   week_no NUMBER(2)
+);
