4107e1e69ce132d985e89a713a95c1d1fd74251e
Adeel Malik <amalik@datical.com>
2024-05-23 10:02:31 -0500

    updated checks to v4.28.0

--- main/700_dml/Q4-2022_employees.sql
+++ main/700_dml/Q4-2022_employees.sql
@@ -1,9 +1,9 @@
 
-INSERT INTO employee (id, name, address1, address2, city, country)
-   VALUES(1, 'Nathan', '5 State St.', '', 'Minneapolis', 'MN');
-INSERT INTO employee (id, name, address1, address2, city, country)
-   VALUES(2, 'Adeel', '201 Park Ave.', '', 'New York', 'NY');
-INSERT INTO employee (id, name, address1, address2, city, country)
-   VALUES(3, 'Annette', '85 Lincoln Blvd.', '', 'Austin', 'TX');
-INSERT INTO employee (id, name, address1, address2, city, country)
-   VALUES(4, 'Lelsey', '8981 Commonwealth Ave.', '', 'Boston', 'MA');
+INSERT INTO employee (id, name, address1, address2, city)
+   VALUES(1, 'Nathan', '5 State St.', '', 'Minneapolis');
+INSERT INTO employee (id, name, address1, address2, city)
+   VALUES(2, 'Adeel', '201 Park Ave.', '', 'New York');
+INSERT INTO employee (id, name, address1, address2, city)
+   VALUES(3, 'Annette', '85 Lincoln Blvd.', '', 'Austin');
+INSERT INTO employee (id, name, address1, address2, city)
+   VALUES(4, 'Lelsey', '8981 Commonwealth Ave.', '', 'Boston');
